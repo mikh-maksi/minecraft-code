749d2bfe4eb6076198f23f932f8870cb3748197d
Mikhail <newlevelcenter@gmail.com>
2021-06-20 01:43:52 +0300

    elements

---
+++ get_elements.js
@@ -0,0 +1,30 @@
+loops.forever(function () {
+    if (blocks.testForBlock(EMERALD_BLOCK, world(51, 5, 47))) {
+        mobs.give(
+        mobs.target(LOCAL_PLAYER),
+        WOODEN_SHOVEL,
+        1
+        )
+    }
+    if (blocks.testForBlock(EMERALD_BLOCK, world(50, 5, 47))) {
+        mobs.give(
+        mobs.target(LOCAL_PLAYER),
+        STICK,
+        1
+        )
+    }
+    if (blocks.testForBlock(EMERALD_BLOCK, world(49, 5, 47))) {
+        mobs.give(
+        mobs.target(LOCAL_PLAYER),
+        IRON_SHOVEL,
+        1
+        )
+    }
+    if (blocks.testForBlock(EMERALD_BLOCK, world(48, 5, 47))) {
+        mobs.give(
+        mobs.target(LOCAL_PLAYER),
+        DIAMOND_SWORD,
+        1
+        )
+    }
+})
