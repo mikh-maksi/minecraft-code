3651335dc92497421e46c0783b0f45a5e8472830
Mikhail <newlevelcenter@gmail.com>
2021-06-20 09:24:41 +0300

    quest_mechanism2

--- project/quest_mechanizm.js
+++ project/quest_mechanizm.js
@@ -10,6 +10,7 @@ player.onItemInteracted(WOODEN_SHOVEL, function () {
     agent.setItem(DIAMOND_BLOCK, 1, 2)
     agent.setItem(STICKY_PISTON, 1, 3)
     agent.setItem(WOODEN_BUTTON, 1, 4)
+    agent.setItem(SIGN, 1, 5)
     agent.setSlot(1)
     for (let index = 0; index < 1; index++) {
         for (let index = 0; index < 4; index++) {
@@ -63,5 +64,26 @@ player.onItemInteracted(WOODEN_SHOVEL, function () {
             agent.move(RIGHT, 1)
         }
     }
-    agent.move(LEFT, 1)
+    agent.move(LEFT, 4)
+    agent.move(UP, 1)
+    for (let index = 0; index < 1; index++) {
+        agent.setSlot(1)
+        for (let index = 0; index < 4; index++) {
+            agent.setSlot(1)
+            agent.place(FORWARD)
+            agent.setSlot(5)
+            agent.place(FORWARD)
+            agent.move(RIGHT, 1)
+        }
+        agent.setSlot(1)
+        agent.move(UP, 1)
+        agent.move(LEFT, 4)
+        for (let index = 0; index < 4; index++) {
+            agent.setSlot(1)
+            agent.place(FORWARD)
+            agent.setSlot(5)
+            agent.place(FORWARD)
+            agent.move(RIGHT, 1)
+        }
+    }
 })
